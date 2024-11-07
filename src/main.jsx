308c7533a40d44afff60ea9980981e0f5e89c1aa
BRENO OLIVEIRA GOMES <BRENO6162@DIGITALCOLLEGE.LOCAL>
2024-11-07 19:09:49 -0300

    instalação das rotas e funcionando

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Login } from './Pages/Login.jsx'
+import { Post } from './pages/Post.jsx'
+import { Home } from './pages/Home.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login/>
+  },
+  {
+    path: "/post",
+    element: <Post/>
+  },
+  {
+    path: "/",
+    element: <Home/>
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
